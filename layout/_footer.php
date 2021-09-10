eb5714e07a74f29ded8e190ea32668586fa6f3e6
Jera Code <juanestebanramirezavila@gmail.com>
2021-09-10 14:11:01 -0500

    Avances footer y newsletter

--- layout/_footer.php
+++ layout/_footer.php
@@ -1,17 +1,104 @@
 <footer>
     <section id="wrap_info_footer" class="container container-center">
-        <div class="col-sm-6 ">
-            <figure class="main_logo">
-            </figure>
-            <div class="main_info">
+        <div class="row">
+            <div class="col-sm-3 ">
+                <a class="main_link" data-toggle="collapse" href="#footerMenu1" role="button" aria-expanded="false" aria-controls="footerMenu1">
+                    Sobre Nosotros
+                </a>
+                <div class="collapse show" id="footerMenu1">
+                    <ul class="footer-list">
+                        <li>
+                            <a href="/quienes-somos">Quienes somos</a>
+                        </li>
+                        <li>
+                            <a href="/sala-de-prensa">Sala de prensa</a>
+                        </li>
+                        <li>
+                            <a href="/trabaja-con-nosotros">Trabaja con Nosotros</a>
+                        <li>
+                            <a href="/premios">Premios</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <a class="main_link" data-toggle="collapse" href="#footerMenu2" role="button" aria-expanded="false" aria-controls="footerMenu1">
+                    Cursos
+                </a>
+                <div class="collapse show" id="footerMenu2">
+                    <ul class="footer-list">
+                        <li>
+                            <a href="/desarrollo">Desarrollo</a>
+                        </li>
+                        <li>
+                            <a href="/marketing">Marketing</a>
+                        </li>
+                        <li>
+                            <a href="/e-commerce">E-Commerce</a>
+                        <li>
+                            <a href="/estrategia-digital">Estrategia Digital</a>
+                        </li>
+                        <li>
+                            <a href="/negocios-digitales">Negocios Digitales</a>
+                        </li>
+                        <li>
+                            <a href="/premios">Diseño</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-sm-3 ">
+                <a class="main_link" data-toggle="collapse" href="#footerMenu3" role="button" aria-expanded="false" aria-controls="footerMenu1">
+                    Contácto
+                </a>
+                <div class="collapse show" id="footerMenu3">
+                    <ul class="footer-list">
+                        <li>
+                            <a href="/oficina">Oficina</a>
+                        </li>
+                        <li>
+                            <a href="/centro-de-Ayuda">Centro de Ayuda</a>
+                        </li>
+                        <li>
+                            <a href="/programa-partners">Programa Partners</a>
+                        <li>
+                            <a href="/programa-de-afiliados">Programa de Afiliados</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <a class="main_link" data-toggle="collapse" href="#footerMenu4" role="button" aria-expanded="false" aria-controls="footerMenu1">
+                    Recursos
+                </a>
+                <div class="collapse show" id="footerMenu4">
+                    <ul class="footer-list">
+                        <li>
+                            <a href="/digital-gift">Digital Gift</a>
+                        </li>
+                        <li>
+                            <a href="/blog">Blog</a>
+                        </li>
+                        <li>
+                            <a href="/comunidad">Comunidad</a>
+                        <li>
+                            <a href="/foro">Foro</a>
+                        </li>
+
+                    </ul>
+                </div>
+
             </div>
         </div>
-        <div class="col-sm-6">
-
-        </div>
-        
     </section>
-    <section id="wrap_links_footer">
+    <section id="wrap_links_footer" class="container  container-center d-flex flex-wrap justify-content-between align-items-end">
+        <a class="main-logo">
+            <img src="/assets/media/logo-yellow.png" alt="Más que partners">
+        </a>
+        <div class="ml-auto">
 
+            <p>Todos los derechos reservados <a class="link" href="masquepartners.com">masquepartners.com</a></p>
+        </div>
     </section>
 </footer>
